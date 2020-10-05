92c5ffd83b74e94ccfca4701debc6138b3a26993
Marathi-small-things <aurangabadpromotion@gmail.com>
2020-10-05 17:34:11 +0530

    first commit

---
+++ js/App.js
@@ -0,0 +1,88 @@
+// auto reload on size change
+
+window.addEventListener('resize', function () { 
+    "use strict";
+    window.location.reload(); 
+});
+
+
+
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+
+
+        burger.addEventListener('click',() => {
+                //toggle Nav
+            nav.classList.toggle('nav-active');
+        
+            //animate links
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = ''
+                } else {
+                    link.style.animation = `navlinkfade 0.5s ease forwards ${index / 5 + 0.3}s`;
+                }
+            });
+            //toggle cross
+        
+            burger.classList.toggle('toggle');
+
+
+        });
+
+    };
+
+
+
+
+navSlide();
+
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+//button
+
+const prevButton = document.querySelector('#prevButton');
+const nextButton = document.querySelector('#nextButton');
+
+//counter 
+let counter = 1;
+let size = carouselImages[1].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' +(-size *counter ) + 'px)'; 
+
+//button listeners
+
+nextButton.addEventListener('click',()=> {
+    if(counter >= carouselImages.length - 1) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' +(-size *counter ) + 'px)';
+});
+
+prevButton.addEventListener('click',()=> {
+    if(counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' +(-size *counter ) + 'px)';
+}); 
+
+carouselSlide.addEventListener('transitionend', ()=>{
+    if(carouselImages[counter].id === 'lastClone'){
+        carouselSlide.style.transition = 'none';
+        counter = carouselImages.length -2 ;
+        carouselSlide.style.transform = 'translateX(' +(-size *counter ) + 'px)';
+    
+    }
+    if(carouselImages[counter].id === 'firstClone'){
+        carouselSlide.style.transition = 'none';
+        counter = carouselImages.length -counter ;
+        carouselSlide.style.transform = 'translateX(' +(-size *counter ) + 'px)';
+    
+    }
+});
+
---
+++ js/contact.js
@@ -0,0 +1,87 @@
+window.addEventListener('resize', function () { 
+    "use strict";
+    window.location.reload(); 
+});
+
+
+
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+
+
+        burger.addEventListener('click',() => {
+                //toggle Nav
+            nav.classList.toggle('nav-active');
+        
+            //animate links
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = ''
+                } else {
+                    link.style.animation = `navlinkfade 0.5s ease forwards ${index / 5 + 0.3}s`;
+                }
+            });
+            //toggle cross
+        
+            burger.classList.toggle('toggle');
+
+
+        });
+
+    };
+
+
+
+
+navSlide();
+
+
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
+  
+    var form = document.getElementById("contact-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
+  
+    // Success and Error functions for after the form is submitted
+  
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "<div class='status'>Submission Success!</div>";
+    }
+  
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem.";
+    }
+  
+    // handle the form submission event
+  
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+  
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
